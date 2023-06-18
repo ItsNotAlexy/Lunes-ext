2fed76b1cd2a657a0f968b84135f08a20feb7aba
Alexyy <lilystreamsup@gmail.com>
2023-06-18 10:28:21 +0700

    Some updates

--- extension.js
+++ extension.js
@@ -2,77 +2,71 @@ const vscode = require('vscode');
 const axios = require('axios');
 
 function activate(context) {
-    let disposable = vscode.commands.registerCommand('lunes.Config', async () => {
-        const apiKey = await vscode.window.showInputBox({
-            placeHolder: "API Key",
-            prompt: "Enter your API Key",
-            value: context.workspaceState.get("lunes.apiKey", "")
-        });
-        const serverID = await vscode.window.showInputBox({
-            placeHolder: "Server ID",
-            prompt: "Enter your Server ID",
-            value: context.workspaceState.get("lunes.serverID", "")
-        });
-
-        if (apiKey && serverID) {
-            context.workspaceState.update("lunes.apiKey", apiKey);
-            context.workspaceState.update("lunes.serverID", serverID);
-            vscode.window.showInformationMessage("Configuration saved successfully!");
-        } else {
-            vscode.window.showErrorMessage("Please enter your API Key and Server ID");
-        }
+  let disposable = vscode.commands.registerCommand('lunes.Config', async () => {
+    const apiKey = await vscode.window.showInputBox({
+      placeHolder: 'API Key',
+      prompt: 'Enter your API Key',
+      value: context.workspaceState.get('lunes.apiKey', '')
+    });
+    const serverID = await vscode.window.showInputBox({
+      placeHolder: 'Server ID',
+      prompt: 'Enter your Server ID',
+      value: context.workspaceState.get('lunes.serverID', '')
     });
 
-    context.subscriptions.push(disposable);
-    
-    vscode.workspace.onDidSaveTextDocument(document => {
-        const KeyIsExist = context.workspaceState.get("lunes.apiKey");
-        const ServerIDIsExist = context.workspaceState.get("lunes.serverID");
-        if (KeyIsExist && ServerIDIsExist) {
-            if (document.uri.scheme === "file") {
-                const fileName = vscode.workspace.asRelativePath(document.uri);
-                const fileExtension = fileName.split('.').pop();
-                vscode.window.showInformationMessage(`Saved: ${fileName} (${fileExtension})`);
+    if (apiKey && serverID) {
+      context.workspaceState.update('lunes.apiKey', apiKey);
+      context.workspaceState.update('lunes.serverID', serverID);
+      vscode.window.showInformationMessage('Configuration saved successfully!');
+    } else {
+      vscode.window.showErrorMessage('Please enter your API Key and Server ID');
+    }
+  });
 
-                const URL = `https://ctrl.lunes.host/api/client/servers/${ServerIDIsExist}/files/write?file=${fileName}`;
-                const RequestHeaders = {
-                    "Content-Type": "application/json",
-                    "Accept": "application/json",
-                    "Authorization": `Bearer ${KeyIsExist}`
-                };
-                const codeContent = document.getText();
+  context.subscriptions.push(disposable);
 
-                const FormatingInfo = vscode.window.showInformationMessage("Formatting code...");
+  vscode.workspace.onDidSaveTextDocument(async (document) => {
+    const KeyIsExist = context.workspaceState.get('lunes.apiKey');
+    const ServerIDIsExist = context.workspaceState.get('lunes.serverID');
 
-                setTimeout(() => {
-                    FormatingInfo.dispose();
-                }, 1000);
-                
-                const formattedCode = codeContent.replace(/"/g, '').replace(/\n/g, '\n    ');
-                const indentedCode = `    ${formattedCode}`;
-                const RequestBody = indentedCode;
+    if (KeyIsExist && ServerIDIsExist) {
+      if (document.uri.scheme === 'file') {
+        const fileName = vscode.workspace.asRelativePath(document.uri);
+        const fileExtension = fileName.split('.').pop();
+        vscode.window.showInformationMessage(`Saved: ${fileName} (${fileExtension})`);
 
-                axios.post(URL, RequestBody, { headers: RequestHeaders })
-                    .then(response => {
-                        if (response.data.error) {
-                            vscode.window.showErrorMessage(response.data.error);
-                        } else {
-                            vscode.window.showInformationMessage("File saved successfully!");
-                        }
-                    })
-                    .catch(error => {
-                        vscode.window.showErrorMessage(error.message);
-                    });
+        const URL = `https://ctrl.lunes.host/api/client/servers/${ServerIDIsExist}/files/write?file=${fileName}`;
+        const RequestHeaders = {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+          Authorization: `Bearer ${KeyIsExist}`
+        };
+        const codeContent = document.getText();
+
+        const requestBody = codeContent.replace(/^"|"$/g, '').replace(/\\r\\n|\\r|\\n/g, '');
+
+        axios
+          .post(URL, requestBody, { headers: RequestHeaders })
+          .then((response) => {
+            if (response.data.error) {
+              vscode.window.showErrorMessage(response.data.error);
+            } else {
+              vscode.window.showInformationMessage('File saved successfully!');
             }
-        } else {
-            vscode.window.showErrorMessage("Please enter your API Key and Server ID");
-        }
-    });
+          })
+          .catch((error) => {
+            vscode.window.showErrorMessage(error.message);
+          });
+      }
+    } else {
+      vscode.window.showErrorMessage('Please enter your API Key and Server ID');
+    }
+  });
 }
 
 function deactivate() {}
 
 module.exports = {
-    activate,
-    deactivate
+  activate,
+  deactivate
 };
